108813f27dcfb1ef50c16af1b979703fabefea47
Ko-HyeJi <hy2ji.k5@gmail.com>
2022-01-17 05:36:36 +0900

    2022_01_16

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,12 @@
 /*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 19:11:04 by hyko              #+#    #+#             */
-/*   Updated: 2022/01/15 22:04:53 by hyko             ###   ########.fr       */
+/*   Updated: 2022/01/17 05:36:10 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
-/*
+
 static char	**ft_malloc_error(char **str)
 {
 	int	i;
@@ -26,68 +25,65 @@ static char	**ft_malloc_error(char **str)
 	free(str);
 	return (NULL);
 }
-*/
-int	word_count(const char *s, char c)
+
+int	word_count(char const *s, char c)
 {
 	int	i;
 	int	count;
 
+	if (s[0] == '\0')
+		return (0);
 	i = 0;
-	count = 1;
-	while (s[i] == c)
-		i++;
-	while (s[i] != '\0')
+	count = 0;
+	if (s[0] != c)
+		count++;
+	while (s[i + 1] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == c && s[i + 1] != c)
 			count++;
-		i++;
+			i++;
 	}
 	return (count);
+}
+
+int	word_len(char const *s, char c, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] != c)
+	{
+		i++;
+		len++;
+	}
+	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**str;
-	int		word_num;
+	int		total_word;
+	int		next_word_len;
 	int		i;
 	int		j;
-	int		k;
 
-	word_num = word_count(s, c);
-	//printf("word_num : %d\n", word_num);
-	str = (char **)malloc(sizeof(char *) * (word_num));
+	total_word = word_count(s, c);
+	str = (char **)malloc(sizeof(char *) * (total_word + 1));
 	if (str == NULL)
 		return (NULL);
 	i = 0;
-	j = 0;
-	while (j < word_num)
+	j = -1;
+	while (++j < total_word)
 	{
 		while (s[i] == c)
 			i++;
-		k = 0;
-		while (s[i] != c && s[i] != '\0')
-		{
-			i++;
-			k++;
-		}
-		if (k == 0)
-			break ;
-		str[j] = (char *)malloc(sizeof(char) * (k + 1));
+		next_word_len = word_len(s, c, i);
+		str[j] = (char *)malloc(sizeof(char) * (next_word_len + 1));
 		if (str[j] == NULL)
-			return (NULL);
-			//return (ft_malloc_error(str));
-		i = i - k;
-		k = 0;
-		while (s[i] != c && s[i] != '\0')
-		{	
-			str[j][k] = s[i];
-			k++;
-			i++;
-		}
-		str[j][k] = '\0';
-		//printf("%s\n", str[j]);
-		j++;
-		i++;
+			return (ft_malloc_error(str));
+		ft_strlcpy(str[j], &s[i], next_word_len + 1);
+		i += next_word_len;
 	}
+	str[j] = NULL;
 	return (str);
 }
